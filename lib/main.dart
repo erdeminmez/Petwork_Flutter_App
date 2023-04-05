98689760ca120d3829edb05ab3d01b5f44b5ddd8
Erdem Inmez <inmez@sheridancollege.ca>
2023-04-05 18:12:17 -0400

    Setting up Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:petwork/responsive/mobile_screen_layout.dart';
 import 'package:petwork/responsive/responsive_layout_screen.dart';
 import 'package:petwork/responsive/web_screen_layout.dart';
 import 'package:petwork/utils/colors.dart';
+import 'package:flutter/foundation.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: FirebaseOptions(
+        apiKey: 'AIzaSyDX7yn8k-UruzK9XsF1HOfueD9N1r_OSBg',
+        appId: '1:195290147414:web:57e2a466a4692aae8ee9f7',
+        messagingSenderId: '195290147414',
+        projectId: 'petwork-92a4c',
+        storageBucket: 'petwork-92a4c.appspot.com',
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
